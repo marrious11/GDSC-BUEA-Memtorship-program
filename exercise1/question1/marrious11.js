008a203238322cb3428856652caab30745792525
soh <sohmarrious@gmail.nom>
2022-12-29 04:57:58 +0100

    exercise[1 to 9] from <marrious11>

--- exercise1/question1/marrious11.js
+++ exercise1/question1/marrious11.js
@@ -1,14 +1,3 @@
 // Variables 
 // qtn1
 let carName="Volvo"
-// qtn2
-let x= 50
-//qtn3
-let sum = 5+10
-console.log(sum);
-//qtn4
-let z,y=0;
-z= x+y
-console.log(z);
-//qtn5
-let fisrtName='John',LastName='Doe',age= 35;
