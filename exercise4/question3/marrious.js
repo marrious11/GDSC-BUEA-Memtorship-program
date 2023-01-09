93e393af136eb3ae284e2286f322d6bba3100521
soh <sohmarrious@gmail.nom>
2023-01-09 18:43:44 +0100

    exo[ 4 to 9] from marrious11

---
+++ exercise4/question3/marrious.js
@@ -0,0 +1,8 @@
+/*Create an object called person with name = John, age = 50.
+Then, access the object to console.log("John is 50").*/
+
+const person= {
+ name: "John", 
+ age: 50,
+};
+Console.log( person['name'] + " is " + person["age"] );
