eae8e16f0fc957eedba1c7c9305589babaeb454b
soh <sohmarrious@gmail.nom>
2023-01-23 20:47:31 +0100

    update

--- exercise8/question4/marrious.js
+++ exercise8/question4/marrious.js
@@ -1,8 +1,7 @@
--//Use the correct Array method to sort the fruits array alphabetically.
+-//Use the correct Array method to sort the elements_fruits array alphabetically.
 
-const fruits = ["Banana", "Orange", "Apple", "Kiwi"];
-var i;
-var x="";
- for(i=0; i<fruits.length; i++){
-   x += fruits[i] + "<br>";
- }
+let elements_fruits =["Banana", "Orange", "Apple", "Kiwi"];
+elements_fruits.sort(function (a, b) {
+  return a.length - b.length;
+});
+console.log(elements_fruits);
